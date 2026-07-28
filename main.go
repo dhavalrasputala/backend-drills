fc33800876578e7fa93fd3d4a07a87346c2a5c35
dhavalrasputala <dhavalrasputala@gmail.com>
2026-07-28 08:48:39 +0530

    -Complete Day2 task

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
+/* Day 1 Task :-
 type MyHandler struct{}
 
 func (h MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -19,3 +15,27 @@ func main() {
 		fmt.Printf("Server Failed :%v", err)
 	}
 }
+*/
+/*
+Day 2 Task:- Task: Using the standard http.ServeMux (Go 1.22+), create routes for GET /users/{id} and POST /users. Extract the id using r.PathValue("id").
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/users/{id}/{name}", AddUserHandler)
+    port := ":8080"
+
+    fmt.Printf("Server Starting at %v\n", port)
+    if err := http.ListenAndServe(port, mux); err != nil {
+        log.Fatal(err)
+    }
+}
+func AddUserHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        http.Error(w, "invalid Method", http.StatusMethodNotAllowed)
+        return
+    }
+    id := r.PathValue("id")
+    name := r.PathValue("name")
+    w.WriteHeader(http.StatusCreated)
+    fmt.Fprintf(w, "ID: %s, Name: %s\n", id, name)
+}
+*/
