06f2aa111cf0118246257d9a7aaa377605b9047b
dhavalrasputala <dhavalrasputala@gmail.com>
2026-08-13 14:32:55 +0530

    GO:modified file

--- GO-Coding/day11/main.go
+++ GO-Coding/day11/main.go
@@ -1,29 +1,37 @@
+// day11:-Task-Create an in-memory map representing an order book (Price -> Volume). Start 10 readers and 1 writer.
+// Notice: Multiple readers can read simultaneously, but writes lock everything.(UNSOLVED)
 package main
 
 import (
-	"crypto/rand"
-	"fmt"
-	"sync"
+	"math/rand"
 )
 
+type NewReader struct {
+}
+
 type stock struct {
-	Price int16
+	Price  int16
 	Volume int16
 }
 
-func newstock(price,volume int16) *stock {
+func newstock(price, volume int16) *stock {
 	return &stock{
-		Price: price,
+		Price:  price,
 		Volume: volume,
 	}
 }
 
-func (s stock) makestocks(num int) {
-	for i=0;i<=num;i++{
-		newstock(s.Price:)
+func makestocks(num int) []*stock {
+	stocks := make([]*stock, 0, num)
+	for i := 0; i < num; i++ {
+		vol := int16(rand.Intn(1000))
+		price := int16(rand.Intn(10000))
+		stocks = append(stocks, newstock(price, vol))
 	}
+	return stocks
 }
 
 func main() {
+	stocs := makestocks(10)
 
 }
